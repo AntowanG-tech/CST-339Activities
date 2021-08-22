21cf0452a16b47d69f3ae5d9e301e80320a6664d
AntowanG-tech <76835658+AntowanG-tech@users.noreply.github.com>
2021-08-22 13:29:49 -0700

    Topic 6-3 final commit

---
+++ topic6-3/src/main/java/com/gcu/business/AnotherOrdersBusinessService.java
@@ -0,0 +1,67 @@
+package com.gcu.business;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.gcu.data.OrderDataService;
+import com.gcu.data.entity.OrderEntity;
+import com.gcu.model.OrderModel;
+
+public class AnotherOrdersBusinessService implements OrdersBusinessInterface {
+	
+	@Autowired
+	OrderDataService service;
+
+	@Override
+	public void test() {
+		// Simply prints "Sup, from the ABS bean" to the console in order to test the interface's functionality
+		System.out.println("Sup, from the ABS bean");
+		
+	}
+
+	@Override
+	public List<OrderModel> getOrders() {
+		
+		//Get all the Entity Orders
+				List<OrderEntity> ordersEntity = service.findAll();
+				
+				//Iterate over the Entity Orders and create a list of Domain Orders
+				List<OrderModel> ordersDomain = new ArrayList<OrderModel>();
+				for(OrderEntity entity : ordersEntity) {
+					ordersDomain.add(new OrderModel(entity.getId(), entity.getOrderNo(), entity.getProductName(), entity.getPrice(), entity.getQuantity()));
+				}
+				
+						
+				//Print orders to the console
+						for (OrderModel orderMod : ordersDomain) {
+							System.out.println(orderMod.toString());
+						}
+				
+				//return List of Domain Orders
+				return ordersDomain;
+		
+		
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		System.out.println("Init() test message AnotherOrdersBusinessService");
+		
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		System.out.println("Destroy() test message AnotherOrdersBusinessService");
+	}
+
+	@Override
+	public OrderModel getOrderById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
